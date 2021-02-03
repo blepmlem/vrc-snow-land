9994c576447ff7f49bf6bf408d09305d4bd16eef
blep <tagendiar+github@outlook.com>
2021-02-03 20:27:41 +0100

    Clean up, create weather manager

---
+++ Assets/Scripts/WeatherManager.cs
@@ -0,0 +1,29 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class WeatherManager : UdonSharpBehaviour
+{
+    [SerializeField]
+    private SnowManager _snowManager;
+
+    [SerializeField]
+    private Camera _camera;
+
+    [SerializeField]
+    private Light _directionalLight;
+
+    [SerializeField]
+    private float _timeOfDay;
+
+    [SerializeField]
+    private Weather[] _weatherTemplates;
+
+    
+    void Start()
+    {
+        
+    }
+}
