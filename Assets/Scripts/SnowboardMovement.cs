60f8cb482019d574acaa312b169841df93dbd0ef
Ro <ro3110@gmail.com>
2021-02-09 23:00:32 +0100

    Improved: Snowboard script, it now uses VRCPlayerApi

--- Assets/Scripts/SnowboardMovement.cs
+++ Assets/Scripts/SnowboardMovement.cs
@@ -1,51 +1,73 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using UdonSharp;
 using UnityEngine;
-using UnityEngine.UI;
-using UdonSharp;
+using VRC.SDKBase;
 
 public class SnowboardMovement : UdonSharpBehaviour
 {
 
+    public Transform StartPosition;
     public LayerMask SlopeMask;
-    public Text DebugText;
-    public float wantedDistanceToGround = 0.02f;
+    public float wantedDistanceToGround = 0.15f;
     public float acceleration = 1f;
     public float friction = 3f;
     public float gravity = 0.5f;
+
+    // The player collider needs to be off the ground otherwise they lag
+    public float playerYOffset = 0.15f;
 
     private Vector2 momentum = Vector2.zero;
     private Vector2 direction = new Vector2(1, 0);
     private float gravityMomentum = 0;
     private bool isFalling = false;
 
- 
+    private VRCPlayerApi localUser;
+
+
+    public override void Interact()
+    {
+        if (localUser == null)
+        {
+            localUser = Networking.LocalPlayer;
+            localUser.TeleportTo(StartPosition.position, StartPosition.rotation);
+            transform.position = StartPosition.position;
+            transform.rotation = StartPosition.rotation;
+        }
+        else
+        {
+            localUser = null;
+        }
+    }
+
 
     // Update is called once per frame
     void Update()
     {
-  
+        if (localUser != null)
+        {
+            SnowboardingUpdate();
+        }
+    }
 
-        DebugText.text = "momentum: " + momentum + ", total speed: "+ momentum.magnitude + ",  transform.eulerAngles.x: " +  transform.eulerAngles.x ;
-
+    private void SnowboardingUpdate()
+    {
         Vector3 movement = transform.forward * momentum.x + transform.right * momentum.y;
         transform.position = transform.position + movement;
 
         RaycastHit hit;
         if (Physics.Raycast(transform.position + Vector3.up, Vector3.down, out hit, 1.2f, SlopeMask))
         {
-           
+
             momentum += Time.deltaTime * direction * acceleration * ScaledToAngle();
             momentum -= Time.deltaTime * momentum * friction;
 
             transform.position = transform.position + ((hit.distance - 1) - wantedDistanceToGround) * Vector3.down;
-            transform.rotation = Quaternion.FromToRotation (transform.up, hit.normal) * transform.rotation;
+            transform.rotation = Quaternion.FromToRotation(transform.up, hit.normal) * transform.rotation;
             gravityMomentum = 0;
             isFalling = false;
         }
         else
         {
-            if(!isFalling)
+            if (!isFalling)
             {
                 gravityMomentum = -movement.y;
                 isFalling = true;
@@ -55,11 +77,16 @@ public class SnowboardMovement : UdonSharpBehaviour
             gravityMomentum += Time.deltaTime * gravity;
             transform.position += Vector3.down * gravityMomentum;
         }
+
+        if (localUser != null)
+        {
+            localUser.TeleportTo(transform.position + Vector3.up * playerYOffset, StartPosition.rotation, VRC_SceneDescriptor.SpawnOrientation.AlignRoomWithSpawnPoint, true);
+        }
     }
 
     private float ScaledToAngle()
     {
-        if(transform.eulerAngles.x > 0 && transform.eulerAngles.x < 90)
+        if (transform.eulerAngles.x > 0 && transform.eulerAngles.x < 90)
         {
             return transform.eulerAngles.x / 90f;
         }
